00095a50f0b88d16eca04051fe9461b47785b124
arjun <arjun.pche@gmail.com>
2022-07-18 13:13:01 +0530

    added counter of number of attemption

--- game.py
+++ game.py
@@ -1,13 +1,15 @@
 import random
 game_over = True
+count = 0 
 random_number = random.randint(1,100)
 while game_over :
     guess_number = int(input("Enter the number between 0 to 100: "))
+    count += 1
     if random_number == guess_number:      
-        print("Congrates !! you win ..!! \n\n")
+        print(f"Congrates !! you win in {count} times!! \n\n")
         break
     elif random_number < guess_number:
         print("Too high ! Guess Again ")
        
     else: 
-          print("Too low ! Guess Again ...")
+        print("Too low ! Guess Again ...")
