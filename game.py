a0abcc8b9c52f0b311f79ad0d85de21fe9007881
arjun <arjun.pche@gmail.com>
2022-07-18 12:48:04 +0530

    improved winning message

--- game.py
+++ game.py
@@ -1,11 +1,10 @@
 import random
 game_over = True
 random_number = random.randint(1,100)
-print(random_number)
 while game_over :
     guess_number = int(input("Enter the number between 0 to 100: "))
     if random_number == guess_number:      
-        print("Congrates !! you win ... in  times")
+        print("Congrates !! you win ..!! \n\n")
         break
     elif random_number < guess_number:
         print("Too high ! Guess Again ")
